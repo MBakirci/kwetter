1f49199ead5bbb4096877c504f894a73b8fefcfa
MBakirci <m.bakirci@outlook.com>
2017-03-23 10:49:56 +0100

    added some bullshit comments

--- src/main/java/com/mehmet/kwetter/dao/UserDao.java
+++ src/main/java/com/mehmet/kwetter/dao/UserDao.java
@@ -1,15 +1,31 @@
 package com.mehmet.kwetter.dao;
 
-import com.mehmet.kwetter.exception.UserNotFoundException;
 import com.mehmet.kwetter.domain.User;
+import com.mehmet.kwetter.exception.UserNotFoundException;
 
 /**
  * Created by Mehmet on 3/10/2017.
  */
 public interface UserDao extends IDao<User> {
+    /**
+     * @param followerId
+     * @param toFollowId
+     * @throws UserNotFoundException
+     */
     void followUser(Long followerId, Long toFollowId) throws UserNotFoundException;
 
+    /**
+     * @param followerId
+     * @param toFollowId
+     * @throws UserNotFoundException
+     */
     void unFollowUser(Long followerId, Long toFollowId) throws UserNotFoundException;
 
+
+    /**
+     * @param username String username of User
+     * @return User object by username
+     * @throws UserNotFoundException
+     */
     User findByUsername(String username) throws UserNotFoundException;
 }
